4f9ce2422b6fc97573de873fbd5bbb7baf11a2a0
mclely <mclely27@gmail.com>
2023-02-17 06:47:56 -0800

    print exacte moi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - thid is the main function
+ * main - this is the main function
  *
  *Return: 0 when successful
  */
 
 int main(void)
 {
-print("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
