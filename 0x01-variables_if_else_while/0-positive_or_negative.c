b19d08c949660d8ab405fbe15944cdb98a268e2a
mclely <mclely27@gmail.com>
2023-02-20 10:10:34 -0800

    blessings

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
 
 	if (n > 0)
 	{
-	printf("%d is positive", n);
+	printf("%d is positive/n", n);
 	}
 	else if (n == 0)
 	{
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-	printf("%d is negative", n);
+	printf("%d is negative\n", n);
 	}
 	return (0);
 }
